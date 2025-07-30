a07c584ae0b909d7734b9c5f742c4c913f7f6b1e
moussacode <samouh269@gmail.com>
2025-07-30 06:27:16 +0000

    Move API routes to web.php to bypass anti-bot protection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SnippetController;
 Route::get('/', function () {
     return view('welcome');
+});
+// Routes pour outils API (Postman, etc.)
+Route::prefix('api')->group(function () {
+    Route::get('/snippets', [SnippetController::class, 'index']);
+    Route::post('/snippets', [SnippetController::class, 'store']);
 });
