52dc08b9cccece5eb73d4596c337f938954ebe01
moussacode <samouh269@gmail.com>
2025-07-30 06:41:05 +0000

    Fix deployment: comment gitignore and add htaccess

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 // Routes pour outils API (Postman, etc.)
-Route::prefix('api')->group(function () {
+Route::prefix('h24api')->group(function () {
     Route::get('/snippets', [SnippetController::class, 'index']);
     Route::post('/snippets', [SnippetController::class, 'store']);
 });
